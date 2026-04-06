254a350bad6e9f6a1bcc19c9c704311f001aa546
James Welbes <james.welbes@gmail.com>
2026-04-05 23:52:15 -0500

    Fix Trash page scrolling: add min-h-0 to flex column container

--- src/pages/Trash.tsx
+++ src/pages/Trash.tsx
@@ -10,7 +10,7 @@ export default function Trash() {
   return (
     <div className="flex h-screen overflow-hidden bg-background">
       <AppSidebar />
-      <div className="flex-1 flex flex-col min-w-0">
+      <div className="flex-1 flex flex-col min-w-0 min-h-0">
         {/* Top bar */}
         <div className="flex items-center gap-2 px-4 h-14 border-b border-border shrink-0">
           {!sidebarOpen && (
